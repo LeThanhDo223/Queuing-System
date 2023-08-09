475716734816ca75f5485ddc879a61e186d6b4cc
cochannelvp <cochannelvp@gmail.com>
2023-08-09 09:21:19 +0700

    firebase login

--- src/login/LoginHome.tsx
+++ src/login/LoginHome.tsx
@@ -1,9 +1,25 @@
-import { Col, Row } from "antd";
-import { Button, Form, Input, Space } from "antd";
-import "../css/Login.css";
-
+import React, { useState } from 'react';
+import { Button, Col, Form, Input, Row, Space } from 'antd';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import '../css/Login.css';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
 
 const LoginHome: React.FC = () => {
+  const [error, setError] = useState<string | null>(null);
+
+  const handleLogin = async (values: any) => {
+    const { username, password } = values;
+    try {
+      const authInstance = getAuth();
+      await signInWithEmailAndPassword(authInstance, username, password);
+
+      // Đăng nhập thành công, thay đổi địa chỉ URL
+      window.location.href = '/menu'; // Thay thế thành đường dẫn bạn muốn chuyển hướng đến
+    } catch (error: any) {
+      setError("Sai mật khẩu hoặc tên đăng nhập");
+    }
+  };
+
   return (
     <>
       <Row>
@@ -13,21 +29,33 @@ const LoginHome: React.FC = () => {
           </div>
 
           <div className="col_login">
-            <Form name="validateOnly" layout="vertical" autoComplete="off">
-              <Form.Item name="username" className="form_item">
+            <Form
+              name="validateOnly"
+              layout="vertical"
+              autoComplete="off"
+              onFinish={handleLogin}
+            >
+              <Form.Item
+                name="username"
+                className="form_item"
+                rules={[{ required: true, message: "Vui lòng nhập email" }]}
+              >
                 <div className="col_text">
-                  <label>Tên đăng nhập *</label>
+                  <label>Tên đăng nhập <Space style={{color:'red'}}>*</Space></label>
                   <Input className="col_input" />
                 </div>
               </Form.Item>
 
-              <Form.Item name="password" className="form_item">
+              <Form.Item
+                name="password"
+                className="form_item"
+                rules={[{ required: true, message: "Vui lòng nhập mật khẩu" }]}
+              >
                 <div className="col_text">
-                  <label>Mật khẩu *</label>
+                  <label>Mật khẩu <Space style={{color:'red'}}>*</Space></label>
                   <Input.Password className="col_input" />
                 </div>
               </Form.Item>
-
               <Form.Item name="remember" className="form_item">
                 <div className="col_text">
                   <Button className="col_qmk" type="link">
@@ -44,6 +72,11 @@ const LoginHome: React.FC = () => {
                 </Space>
               </Form.Item>
             </Form>
+            {error && (
+              <div style={{ color: 'red' }} className="error-message">
+                <ExclamationCircleOutlined  /> {error}
+              </div>
+            )}
           </div>
         </Col>
         <Col className="col_bg2" span={14}>
